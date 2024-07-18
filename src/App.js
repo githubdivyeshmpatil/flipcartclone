54ded5fae067f1018c58e54aa06fa6ef42f85d64
githubdivyeshmpatil <githubdivyeshmpatil@gmail.com>
2024-07-18 17:44:54 +0530

    navbar new change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
-import Category from './Components/Category/Category';
-import Single_productcompo from './Components/Singleproduct/Single_productcompo';
+
 
 function App() {
   return (
@@ -16,8 +15,7 @@ function App() {
 
     <Route exact path='/' element={<Home/>}/>
       <Route path='/about-us' element={<About/>}/>
-      <Route path='/category/:id' element={<Category/>}/>
-      <Route path='/product/:id' element={<Single_productcompo/>}/>
+      
 
       
     
--- src/Components/Cart/Serch.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Serch() {
-  return (
-    <div>Serch</div>
-  )
-}
-
-export default Serch
