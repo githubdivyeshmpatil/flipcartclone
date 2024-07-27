7bdf054ccca42dcf4f860eba94e5feea3383e4fd
githubdivyeshmpatil <githubdivyeshmpatil@gmail.com>
2024-07-27 12:30:12 +0530

    puma

--- src/Components/Features Brands/CartNavbar.js
+++ src/Components/Features Brands/CartNavbar.js
@@ -7,9 +7,10 @@ const CartNavbar = ({ toggleCart }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light ">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Navbar
-    </a>
+  <button className="cart-button" onClick={toggleCart}>
+                <FaShoppingCart  color="white" size={25}/>
+               
+              </button>
     <button
       className="navbar-toggler"
       type="button"
@@ -24,11 +25,6 @@ const CartNavbar = ({ toggleCart }) => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link " aria-current="page" href="#">
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
         <form className="d-flex">
         <input
           className="form-control me-2"
@@ -39,12 +35,7 @@ const CartNavbar = ({ toggleCart }) => {
         
       </form>
         </li>
-        <li className="nav-item">
-        <button className="cart-button" onClick={toggleCart}>
-                <FaShoppingCart />
-                Cart
-              </button>
-        </li>
+        
        
       
          
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,9 @@ import { Container } from 'react-bootstrap'
 import Home_page_slider from './Home_page_slider';
 import Product_list_home from '../Product_list_home.js/Product_list_home';
 import Features from './Features Brands/Features';
+import leg from './assets/leg.jpg'
+import vidiosrc from './assets/vidio.mp4'
+
 
 
 
@@ -38,6 +41,20 @@ function Home() {
       <Home_page_slider/>
       <Product_list_home/>
       <Features/>
+      <div className="container" id='puma'>
+      <div className="columnnn">
+        
+        <video autoPlay muted controls>
+          <source src={vidiosrc} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="columnnn">
+        <img src={leg} alt="Image" />
+       
+      </div>
+    </div>
+
    
       
      
